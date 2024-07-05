33b05cd5420793206fe19da49ff7a3a39784bed0
phuwadech <aofphuwadech@gmail.com>
2024-07-05 17:08:25 +0700

    edit Point

--- src/pages/Point.jsx
+++ src/pages/Point.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import Content from "../layouts/Content.jsx";
 import DataTable from "react-data-table-component";
 import {data} from "../services/data.jsx";
